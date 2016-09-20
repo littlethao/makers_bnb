f503ba5246c3aed7b1d63a0d73dbc19046ed2096
Rosie Allott <rosie.allott@gmail.com>
2016-09-20 16:44:26 +0100

    User logs in and saved to database

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 var http = require('http');
 var fs = require('fs');
+var qs = require('querystring');
+// var NodeSession = require('node-session');
+var knex = require('./db/knex.js');
 
 var server = http.createServer(function (req, res){
+  var bookshelf = require('bookshelf')(knex);
+  var User = bookshelf.Model.extend({tableName: 'users'});
+
+  // session = new NodeSession({secret: 'murtzsecretkey'});
 
   if (req.url == "/") {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Hello World!</h1>');
+    res.write('Hello World! Welcome rosie@allott.com');
     res.end();
   }
 
-  else if (req.url == '/users/new') {
+  else if (req.url == '/users/new' && req.method == 'GET') {
     fs.readFile('./views/users/new.html', 'UTF-8', function(err, html){
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(html);
     });
+
+  }
+
+  else if (req.url == '/users/new' && req.method == 'POST') {
+    var body = '';
+    req.on('data', function(data){
+      body += data;
+    });
+
+      req.on('end', function(){
+        var params = qs.parse(body);
+        new User({email: params.email, password: params.password}).save();
+        res.writeHead(302, {Location: '/'});
+        res.end();
+      });
 
   }
 
--- spec/zombie.spec.js
+++ spec/zombie.spec.js
@@ -1,25 +1,48 @@
+process.env.NODE_ENV = 'test';
+
 var Browser = require("zombie");
 var url = "http://localhost:8000";
 var browser = new Browser();
+var knexCleaner = require('knex-cleaner');
+var knex = require('../db/knex.js');
 
-describe("users/new", function(){
-  it("should have defined headless browser", function(next){
-    expect(typeof browser != 'undefined').toBe(true);
-    expect(browser instanceof Browser).toBe(true);
-    next();
+describe('user testing', function(){
+
+  beforeEach(function(){
+    knexCleaner.clean(knex);
   });
-});
 
-
-describe("users/new", function(){
-  it("should have status code of 200", function(next){
-    browser.visit(url + '/users/new', function(err){
-      expect(browser.statusCode).toEqual(200);
-      expect(browser.html('body')).toContain('Signup');
-      expect(browser.query('#email-address')).toBeDefined();
-      expect(browser.query('#password')).toBeDefined();
-      expect(browser.query('#signup')).toBeDefined();
+  describe("users/new", function(){
+    it("should have defined headless browser", function(next){
+      expect(typeof browser != 'undefined').toBe(true);
+      expect(browser instanceof Browser).toBe(true);
       next();
+    });
+  });
+
+
+  describe("users/new", function(){
+
+    it("should have status code of 200", function(next){
+      browser.visit(url + '/users/new', function(err){
+        expect(browser.statusCode).toEqual(200);
+        expect(browser.html('body')).toContain('Signup');
+        expect(browser.query('#email-address')).toBeDefined();
+        expect(browser.query('#password')).toBeDefined();
+        expect(browser.query('#signup')).toBeDefined();
+        next();
+      });
+    });
+    it("submitting should create a success message", function(next){
+      browser.visit(url + '/users/new', function(err){
+        browser.fill('#email', 'rosie@allott.com');
+        browser.fill('#password', 'password');
+        browser.pressButton('#signup', function(){
+          expect(browser.statusCode).toEqual(200);
+        // expect(browser.html('body')).toContain('Welcome rosie@allott.com');
+          next();
+        });
+      });
     });
   });
 });
