348712fc1578add9935e3c569a652714c7db1db9
TomStuart92 <Thomascstuart@gmail.com>
2016-09-23 14:54:34 +0100

    Fix Merge Issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,43 +1,7 @@
 var userController = function(req,res) {
-  if (req.url == '/users/new' && req.method == 'GET' ) {
-    fs.readFile('./views/users/new.ejs', 'UTF-8', function(err, html){
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(header);
-      res.write(html);
-      res.end();
-    });
-  }
 
-  else if (req.url == '/users/new' && req.method == 'POST') {
-    var body = '';
-    req.on('data', function(data){
-      body += data;
-    });
 
-    req.on('end', function(){
-      var params = qs.parse(body);
-      bcrypt.hash(params.password, 10, function(err, hash){
-        User.forge({email: params.email, password: hash}).save().then(function(user){
-            req.session.put('id', user.toJSON().id);
-            req.session.flash('message', 'Welcome ' + user.toJSON().email);
-            res.writeHead(302, {Location: '/spaces'});
-            res.end();
-        });
-      });
-    });
-  }
-
-  else if (req.url == '/users/login' && req.method == 'GET' && !req.session.get('id')) {
-    fs.readFile('./views/users/login.ejs', 'UTF-8', function(err, page){
-      var html = ejs.render(page, {message: req.session.get('message')});
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(header);
-      res.write(html);
-      res.end();
-    });
-  }
-
-else if (req.url == '/users/new' && req.method == 'GET' ) {
+if (req.url == '/users/new' && req.method == 'GET' ) {
   if (req.session.get('id')) {
     res.writeHead(302, {Location: '/spaces'});
     res.end();
@@ -46,7 +10,9 @@ else if (req.url == '/users/new' && req.method == 'GET' ) {
     fs.readFile('./views/users/new.ejs', 'UTF-8', function(err, page){
       var html = ejs.render(page, {message: req.session.get('message')});
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(html);
+      res.write(header);
+      res.write(html)
+      res.end();
     });
   }
 }
@@ -82,6 +48,7 @@ else if (req.url == '/users/login' && req.method == 'GET') {
     fs.readFile('./views/users/login.ejs', 'UTF-8', function(err, page){
       var html = ejs.render(page, {message: req.session.get('message')});
       res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(header)
       res.write(html);
       res.end();
     });
@@ -92,25 +59,6 @@ else if (req.url == '/users/login' && req.method == 'GET') {
     res.end();
   }
 }
-
-else if (req.url == '/users/login' && req.method == 'POST') {
-  var body = '';
-  req.on('data', function(data){
-    body += data;
-  });
-
-  req.on('end', function(){
-    var params = qs.parse(body);
-    User.where({email: params.email}).fetch().then(function(user){
-      if (user && bcrypt.compareSync(params.password, user.toJSON().password)) {
-        req.session.put('id', user.toJSON().id);
-        req.session.flash('message', 'Successfully logged in');
-        res.writeHead(302, {Location: '/spaces'});
-        res.end();
-        }
-      });
-    });
-  }
 
   else if (req.url == '/users/login' && req.method == 'POST') {
     var body = '';
